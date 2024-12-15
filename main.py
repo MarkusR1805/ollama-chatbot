f5135910a600b65d26c9cb8a02c5b00a6c8289e1
Markus Rößler <info@der-zerfleischer.de>
2024-12-15 08:32:56 +0100

    Design

--- main.py
+++ main.py
@@ -52,6 +52,9 @@ class App(QWidget):
                 font-size: 16px;
                 color: rgb(215, 215, 215);
                 padding: 5px;
+                border: 2px solid rgb(31, 84, 30);
+                border-radius: 15px;
+                background-color: rgb(30, 30, 30);
             }
             QLabel {
                 font-size: 16px;
